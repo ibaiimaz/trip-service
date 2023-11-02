725ebf5fe995d3807b798e2ffcc9b839b13e6496
ibai imaz <ibaiimaz@gmail.com>
2023-11-02 19:04:24 +0100

    test cleanup

--- tests/TripService.test.ts
+++ tests/TripService.test.ts
@@ -10,46 +10,51 @@ describe("TripService", () => {
     const ANOTHER_USER = new User();
     const TO_LONDON = new Trip();
     const TO_PARIS = new Trip();
+
     let loggedInUser: User | null;
+    let tripService: TripService;
 
-    it("should validate logged in user", () => {
-        const tripService = new TestableTripService();
-
-        loggedInUser = GUEST;
-
-        expect(() => tripService.getTripsByUser(ANY_USER)).toThrow(UserNotLoggedInException)
+    beforeEach(() => {
+        tripService = new TestableTripService();
     });
 
-    it("should return no trips when the users are not friends", () => {
-        const tripService = new TestableTripService();
+    describe("when user not logged in", () => {
+        beforeEach(() => {
+            loggedInUser = GUEST;
+        });
 
-        loggedInUser = REGISTERED_USER;
-
-        const stranger = new User();
-        stranger.addFriend(ANOTHER_USER);
-        stranger.addTrip(TO_LONDON);
-
-        const trips = tripService.getTripsByUser(stranger);
-
-        expect(trips).toHaveLength(0);
+        it("should validate logged in user", () => {
+            expect(() => tripService.getTripsByUser(ANY_USER)).toThrow(UserNotLoggedInException)
+        });
     });
 
-    it("should return trips when the users are friends", () => {
-        const tripService = new TestableTripService();
+    describe("when user logged in", () => {
+        beforeEach(() => {
+            loggedInUser = REGISTERED_USER;
+        });
 
-        loggedInUser = REGISTERED_USER;
-
-        const friend = new User();
-        friend.addFriend(REGISTERED_USER);
-        friend.addFriend(ANOTHER_USER);
-        friend.addTrip(TO_LONDON);
-        friend.addTrip(TO_PARIS);
-
-        const trips = tripService.getTripsByUser(friend);
-
-        expect(trips).toEqual([TO_LONDON, TO_PARIS]);
+        it("should return no trips when the users are not friends", () => {
+            const stranger = new User();
+            stranger.addFriend(ANOTHER_USER);
+            stranger.addTrip(TO_LONDON);
+    
+            const trips = tripService.getTripsByUser(stranger);
+    
+            expect(trips).toHaveLength(0);
+        });
+    
+        it("should return trips when the users are friends", () => {
+            const friend = new User();
+            friend.addFriend(REGISTERED_USER);
+            friend.addFriend(ANOTHER_USER);
+            friend.addTrip(TO_LONDON);
+            friend.addTrip(TO_PARIS);
+    
+            const trips = tripService.getTripsByUser(friend);
+    
+            expect(trips).toEqual([TO_LONDON, TO_PARIS]);
+        });
     });
-
 
     class TestableTripService extends TripService {
         protected loggedInUser(): User | null {
